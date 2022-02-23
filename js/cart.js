113f8758e2cda28aaf522387427acac5aa344f65
Svyatoslav <svyatoslavlp@mail.ru>
2022-02-24 00:35:53 +0500

    Сделано задание 1 дня - простое

---
+++ js/cart.js
@@ -0,0 +1,14 @@
+const cart = function () {
+   const cartBtn = document.querySelector('.button-cart')
+   const cart = document.querySelector('#modal-cart')
+   const closeBtn = cart.querySelector('.modal-close')
+
+   cartBtn.addEventListener('click', function () {
+      cart.style.display = 'flex'
+   })
+   closeBtn.addEventListener('click', function () {
+      cart.style.display = ''
+   })
+}
+
+cart()
