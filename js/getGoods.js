72b976184dabcdb8a46f38ce0e2265abe7cd288c
Svyatoslav <svyatoslavlp@mail.ru>
2022-02-26 23:36:55 +0500

    Работает вывод карточек

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,6 +1,35 @@
 const getGoods = () => {
    const links = document.querySelectorAll('.navigation-link')
 
+   const renderGoods = (goods) => {
+      const goodsContainer = document.querySelector('.long-goods-list')
+
+      goodsContainer.innerHTML = ""
+
+      goods.forEach(good => {
+         const goodBlock = document.createElement('div')
+
+         goodBlock.classList.add('col-lg-3')
+         goodBlock.classList.add('col-sm-6')
+
+         goodBlock.innerHTML = `
+            <div class = "goods-card">
+               <span class = "label ${good.label ? null : 'd-none'}">${good.label}</span>
+               <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+               <h3 class="goods-title">${good.name}</h3>
+               <p class = "goods-description">${good.description}</p>
+               <button class = "button goods-card-btn add-to-cart" data-id="${good.price}">
+               <span class = "button-price">$${good.price}</span>
+               </button>
+            </div>
+         `
+
+         goodsContainer.append(goodBlock)
+
+         console.log(good);
+      })
+   }
+
    const getData = (value, category) => {
       fetch('/db/db.json')
          .then((res) => res.json())
@@ -8,6 +37,12 @@ const getGoods = () => {
             const array = category ? data.filter((item) => item[category] === value) : data
 
             localStorage.setItem('goods', JSON.stringify(array)) // записываем в влокальное хранилище браузера пользователя в формате string
+
+            if (window.location.pathname !== '/goods.html') {
+               window.location.href = '/goods.html'
+            } else {
+               renderGoods(array)
+            }
          })
    }
 
@@ -20,12 +55,10 @@ const getGoods = () => {
          getData(linkValue, category)
       })
    })
-   /*
-      localStorage.setItem('goods', JSON.stringify([1, 2, 3]))
 
-      const goods = JSON.parse(localStorage.getItem('goods'))
-      console.log(goods);
-   */
+   if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
+      renderGoods(JSON.parse(localStorage.getItem('goods')))
+   }
 }
 
 getGoods()
